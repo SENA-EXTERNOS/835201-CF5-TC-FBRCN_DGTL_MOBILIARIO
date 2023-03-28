dda9018924b6cd65e2c83abcb866d9876d8656d6
elrilacho <camilobr17@gmail.com>
2023-03-28 17:11:20 -0500

    CF5 listo

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,9 +7,9 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
-        .titulo-segundo
+        .titulo-segundo.color-acento-contenido
           h2 Fundamentos de impresión 3D
         p Esta actividad le permitirá determinar el grado de apropiación de los contenidos del componente formativo.
 
